7c86e02e9479e0c060e0a6a3d799cfc859d55fe2
Analee Maharaj <120334807+analees@users.noreply.github.com>
2025-02-27 14:10:22 -0600

    Add files via upload

---
+++ OptionsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  OptionsViewController.swift
+//  trendrise
+//
+//  Created by Analee Maharaj on 12/1/24.
+//
+
+import UIKit
+
+class OptionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+        
+    @IBOutlet weak var takepicbtn: UIImageView!
+    
+
+    
+    
+    
+}
